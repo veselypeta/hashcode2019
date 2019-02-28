699202f308f157800aad92bd0de67c4399abb5e1
veselypeta <veselypeta@gmail.com>
2019-02-28 18:53:37 +0000

    vector calculated

--- Photo.py
+++ Photo.py
@@ -6,6 +6,14 @@ Created on Thu Feb 28 17:42:24 2019
 @author: aimeeredbond
 """
 
+def vocabulary(photos):
+    vocabulary = []
+    for photo in photos:
+        for tag in photo.tags_list:
+            if tag not in vocabulary:
+                vocabulary.append(tag)
+    return vocabulary
+
 class Photo:
     def __init__(self, photo_id, orientation, no_tags, tags_list):
         self.photo_id = photo_id
@@ -15,7 +23,13 @@ class Photo:
     
     def __str__(self):
         return "Photo with id = {}, orientation = {} and n_tags = {}".format(self.photo_id, self.orientation, self.no_tags)
-        
+    
+    def vector(self, vocabulary):
+        vector = [0] * len(vocabulary)
+        for i, tag in enumerate(vocabulary):
+            if tag in self.tags_list:
+                vector[i] = 1
+        return vector
         
     
     
