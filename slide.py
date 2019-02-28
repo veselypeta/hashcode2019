e688c4aebdda4277b5e34fa590e179d70127b941
veselypeta <veselypeta@gmail.com>
2019-02-28 19:36:39 +0000

    fix bracket

--- slide.py
+++ slide.py
@@ -2,13 +2,7 @@
 # A slide contains a list of photos. The list can either contain one horizontal
 # Or two vertical photos, and also stores a list of their combined tags
 class Slide():
-    def __init__(self,photo1,photo2:
-        # self.photos = [photo1]
-        # self.tags = photo1.tags_list
-        # if photo2 is not None:
-        #     self.photos.append(photo2)
-        #     self.tags.append(photo2.tags_list)
-
+    def __init__(self,photo1,photo2):
         self.left_photo = photo1
         self.right_photo = photo2
         self.tags = self.left_photo.tags_list
@@ -17,10 +11,11 @@ class Slide():
                 self.tags.append(t)
 
 def create_vertical_slides(verticals):
-    v_slides = []
+    v_front = []
+    v_back = []
     for i in range(len(verticals)):
         for j in range(i, len(verticals)):
-            s = Slide([verticals[i],verticals[j]])
+            s = Slide(verticals[i], verticals[j])
             v_slides.append(s) 
     return v_slides
 
