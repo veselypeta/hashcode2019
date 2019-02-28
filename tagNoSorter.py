771e4c6b2c4bcca5de0c1b825c251135ce909cd6
fraserdscott <fraserdscott@gmail.com>
2019-02-28 18:42:21 +0000

    Update tagNoSorter.py

--- tagNoSorter.py
+++ tagNoSorter.py
@@ -1,6 +1,6 @@
 from slide import Slide
 
-#Takes a list of slides and returns a list of list based on the size of tags
+#Takes a list of slides and returns a list of lists based on the size of tags
 def tagNoSorter(slides):
     maxTagNo = 0
     for slide in slides:
