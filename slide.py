7114f4e0844ac6f0481b05373afd9e6da728a5f4
fraserdscott <fraserdscott@gmail.com>
2019-02-28 18:30:22 +0000

    Slide class and sort by tag number

---
+++ slide.py
@@ -0,0 +1,11 @@
+
+#This class takes a list of photos which will have length one or two
+#And stored them as well as their combined tags
+class Slide():
+    def __init__(self,photos):
+        self.photos = photos
+        self.tags = []
+        self.tags.append(photos[0].tags)
+        if len(photos) == 2:
+            self.tags.append(photos[1].tags)
+            
