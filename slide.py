ea0ee16072234d665faba7f8529b4869fec5c870
veselypeta <veselypeta@gmail.com>
2019-02-28 19:02:54 +0000

    nothing

--- slide.py
+++ slide.py
@@ -3,8 +3,16 @@
 #And stored them as well as their combined tags
 class Slide():
     def __init__(self,photos):
-        self.photos = photos
+        self.photos = photos # two photos
         self.tags = []
         self.tags.append(photos[0].tags)
         if len(photos) == 2:
-            self.tags.append(photos[1].tags)            
+            self.tags.append(photos[1].tags)
+
+        self.n_tags = len(self.tags) 
+
+                      
+
+
+def create_vertical_slides(verticals):
+    for
