2d09cb61b0913e44c2a6a218fed9dc0955e80e2e
veselypeta <veselypeta@gmail.com>
2019-02-28 18:19:11 +0000

    parse finnished

---
+++ parse.py
@@ -0,0 +1,23 @@
+
+
+def parseData(filename):
+    with open(filename, 'r') as inputFile:
+        lines = inputFile.readlines()
+        lines = [x[0:-1] for x in lines]
+        n = int(lines[0])
+        for i in range(1,n):
+            photo = lines[i].split(' ')
+            orientation = photo[0]
+            n_tags = int(photo[1])
+            tags = []
+            for j in range(2, n_tags):
+                tags.append(photo[j])
+            
+            
+
+
+
+
+
+
+parseData("a_example.txt")
