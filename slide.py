ba5045c6e9b2fbeeb99424fcabbb928e5c29eb4a
Fraser Scott <fraserdscott@gmail.com>
2019-02-28 19:07:55 +0000

    changed constructor

--- slide.py
+++ slide.py
@@ -1,16 +1,17 @@
 
-#This class takes a list of photos which will have length one or two
-#And stored them as well as their combined tags
+# A slide contains a list of photos. The list can either contain one horizontal
+# Or two vertical photos, and also stores a list of their combined tags
 class Slide():
-    def __init__(self,photos):
-        self.photos = photos # two photos
-        self.tags = []
-        self.tags.append(photos[0].tags)
-        if len(photos) == 2:
-            self.tags.append(photos[1].tags)
+    def __init__(self,photo1,photo2=None):
+        self.photos = [photo1]
+        self.tags = [photo1.tags]
+        if photo2 != None:
+            self.photos.append(photo2)
+            self.tags.append(photo2.tags)
 
         self.n_tags = len(self.tags) 
 
+    def 
                       
 
 
