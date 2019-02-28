6d34723648be8bf2ca7f3290475555c7fd943789
veselypeta <veselypeta@gmail.com>
2019-02-28 20:49:24 +0000

    added output functionality

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def output_file(slideshow):
     with open("we_are_the_champions.txt", 'w') as myFile:
         n = len(slideshow)
         myFile.write(str(n) + "\n")
-            
+        for slide in slideshow:
+            if isinstance(slide, Vertical_Photo):
+                myFile.write(str(slide.left_photo.photo_id) + " " + str(slide.right_photo.photo_id) + "\n")
+            if isinstance(slide, Photo):
+                myFile.write(str(slide.photo_id) + "\n")
+
 
 photos = parseData("d_pet_pictures.txt")
 verticals = [x for x in photos if x.orientation=='V']
@@ -15,4 +20,4 @@ verticalSlides = create_vertical_slides(verticals)
 
 all_photos = horizontals + verticalSlides
 
-print(len(all_photos))
+output_file(verticalSlides)
